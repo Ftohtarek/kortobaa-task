9568731f4615f9a59edc36164629b480fc876e58
unknown <ftohtarek48@gmail.com>
2023-02-27 01:45:01 +0200

    add rating star actions

--- src/app/shared/component/card/card.component.ts
+++ src/app/shared/component/card/card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'productCard',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements AfterViewInit {
+  @ViewChild('rating') rating?: ElementRef;
 
+  constructor(private render: Renderer2) { }
+  ngAfterViewInit(): void {
+
+  }
+  setRating(index: Number) {
+    const ratingElement: HTMLElement[] = Array.from(this.rating?.nativeElement.children)
+    ratingElement.forEach((element, i) => index <= i ?
+      this.render.setStyle(element, 'color', '#ffe621') : this.render.setStyle(element, 'color', '#eaeaea')
+    );
+  }
 }
