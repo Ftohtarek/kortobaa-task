7b57363067626546a1121b70fb3ae211f8b85f05
unknown <ftohtarek48@gmail.com>
2023-02-27 16:49:59 +0200

    change in search navbar behaviour by making it sticky while scrolling and add box shadow apply if is not stiky to top

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  navbarToggle: boolean = true
+  TimeOut: any;
+  scrollTop: any;
+  onWindowScroll() {
+    this.scrollTop = document.documentElement.scrollTop
+    clearTimeout(this.TimeOut)
+    if (this.scrollTop < 40)
+      return this.navbarToggle = true;
 
+    this.navbarToggle = true
+    return this.TimeOut = setTimeout(() => {
+      this.navbarToggle = false;
+    }, 3000);
+
+  }
 }
