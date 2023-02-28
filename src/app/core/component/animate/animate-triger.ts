8447edc1614de91c9f82fefbee0198f804f99967
unknown <ftohtarek48@gmail.com>
2023-02-28 13:38:10 +0200

    build slider to caption background image

--- src/app/core/component/animate/animate-triger.ts
+++ src/app/core/component/animate/animate-triger.ts
@@ -20,6 +20,7 @@ export const slideInOutAnimation = trigger('slideInOut', [
     }))
   ]
   )]),
+  
   transition('out => in', [group([
     animate('1ms ease-in-out', style({
       'visibility': 'visible'
@@ -39,7 +40,7 @@ export const swapAnimation = trigger('swap', [
     'opacity': '1', 'display': 'block', 'transform': 'scale3d(1)'
   })),
   state('out', style({
-    'opacity': '0', 'display': 'none','transform': 'scale3d(0.1, 0.1, 0.1)' 
+    'opacity': '0', 'display': 'none', 'transform': 'scale3d(0.1, 0.1, 0.1)'
   })),
 
   transition('in => out', [
@@ -57,7 +58,7 @@ export const swapAnimation = trigger('swap', [
     )]),
 
   transition('out => in', [
-    style({ 'display': 'block'}),
+    style({ 'display': 'block' }),
     group([
       animate('500ms ease-in-out', style({
         'opacity': '1'
@@ -68,23 +69,74 @@ export const swapAnimation = trigger('swap', [
       ),
     ])
   ]
-
   )
-
-
-  // transition('out => in', [group([
-  //   animate('1ms ease-in-out', style({
-  //     'display': 'block'
-  //   })),
-  //   animate('10ms ease-in-out', style({
-  //     'visibility': 'visible'
-  //   })),
-  //   animate('700ms ease-in-out', style({
-  //     'max-width': '100%'
-  //   })),
-  //   animate('4000ms ease-in-out', style({
-  //     'opacity': '1'
-  //   }))
-  // ]
-  // )])
 ]);
+
+export const backoutAnimation = trigger('backOut', [
+  state('inital', style({ 'opacity': '1', 'transform': 'translateX(0px) scale(1)' })),
+
+  state('left', style({ 'opacity': '1' })),
+
+  state('right', style({ 'opacity': '0' })),
+
+  transition('inital => right', [
+    group([
+      animate('300ms ease-in-out', style({
+        'transform': ' translateX(0px) ',
+        'opacity': ' 0.7'
+      })),
+
+      animate('600ms ease-in-out', style({
+        'transform': ' translateX(100%) scale(0.0)',
+        'opacity': ' 0.2'
+      })),
+
+    ]
+    )]),
+
+  transition('inital => left', [
+    group([
+      animate('300ms ease-in-out', style({
+        'transform': ' translateX(0px) ',
+        'opacity': ' 0.7'
+      })),
+
+      animate('600ms ease-in-out', style({
+        'transform': ' translateX(-100%) scale(0.2)',
+        'opacity': ' 0.2'
+      })),
+
+    ]
+    )]),
+]);
+
+export const backInAnimation = trigger('backIn', [
+
+  transition('inital=> right', [
+    style({ ' transform': 'translateX(100%) scale(0)' }),
+
+    group([
+      animate('300ms ease-in-out', style({
+        'transform': ' translateX(-100%) scale(0.3)',
+      })),
+
+      animate('600ms ease-in-out', style({
+        'transform': 'scale(1)',
+      })),
+    ]
+    )]),
+
+  transition('inital => left', [
+    style({ ' transform': 'translateX(-100%) scale(0)' }),
+    group([
+      animate('300ms ease-in-out', style({
+        'transform': ' translateX(100%) scale(0.7)',
+      })),
+
+      animate('600ms ease-in-out', style({
+        'transform': 'scale(1)',
+      })),
+    ]
+    )]),
+]);
+
--- src/app/core/component/home/home.component.ts
+++ src/app/core/component/home/home.component.ts
@@ -2,20 +2,22 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MockService } from 'src/app/shared/mock/mock.service';
 import { Product, Products } from 'src/app/shared/models/product';
 import { CdkLayoutService } from 'src/app/shared/service/cdk-layout.service';
-import { swapAnimation } from '../animate/animate-triger';
+import { swapAnimation, backoutAnimation, backInAnimation } from '../animate/animate-triger';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [swapAnimation]
+  animations: [swapAnimation, backoutAnimation, backInAnimation]
 })
 export class HomeComponent implements OnInit, AfterViewInit {
   products: Product[] = <Product[]>[];
   productCategories = { new: 'new', all: 'all', lastView: 'lastView' }
   activeCatergories = this.productCategories.new;
   smallSize: boolean = false;
-
+  slide = false;
+  direction = 'inital'
+  timeOut: any;
   constructor(private productService: MockService, private layout: CdkLayoutService) { }
 
   ngOnInit(): void {
@@ -31,5 +33,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
         this.smallSize = false
       }
     })
+  }
+
+  bgCaptionImg(direction: string) {
+    // clearTimeout(this.timeOut)
+    this.direction = direction
+    this.slide = false
+    
+    // // to make change in state of animation sensor
+    this.timeOut = setTimeout(() => {
+      // this mean return to initial state 
+      this.slide = true
+    }, 600);
   }
 }
